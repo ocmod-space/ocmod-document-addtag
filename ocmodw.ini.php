c0370ae809fe5523efa34395f4961ffaf2bc7fdd
akaunderr <akaunderr@gmail.com>
2024-05-29 16:16:01 +0300

    updated module wrapper

---
+++ ocmodw.ini.php
@@ -0,0 +1,15 @@
+<?php
+
+define('VERSION', '2.2.1');
+define('YEAR', '2021');
+
+define('AUTHOR', 'Andrii Burkatskyi');
+define('EMAIL', 'ocmod.space@gmail.com');
+
+define('MODLINK', 'https://github.com/ocmod-space/ocmod-document-addtag');
+define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-document-addtag');
+define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+
+define('LICENSE', 'https://raw.githubusercontent.com/ocmod-space/ocmod-document-addtag/main/LICENSE.txt');
+
